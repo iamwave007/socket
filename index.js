4d2d950f7719e693819f445c998be9f7e799cf9e
iamwave007 <iamwave007@hotmail.com>
2015-01-06 18:52:43 -0500

    more exploring socket.io

--- index.js
+++ index.js
@@ -11,16 +11,42 @@ app.get('/',function(req,res){
 });
 
 io.on('connection',function(socket){
-  socket.on('james',function(msg){
+  socket.on('james',function(msg,second){
     io.emit('james', msg);
+    console.log(second);
   });
 
-  socket.on('disconnect',function(){
-
+  socket.on('disconnect',function(dd){
+    console.log(dd)
+    console.log("disconnected");
   });
+
+  socket.broadcast.emit("hello");
+  //io.emit("james","io.emit works")
+
 });
 
+// io.sockets.emit('hi','everyone');
 
-http.listen(3000,function(){
-  console.log("listening here! 3000");
+
+http.listen(8000,function(){
+  console.log("listening here! 8000");
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
