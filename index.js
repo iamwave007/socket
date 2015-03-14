952d8cf09995bc1b3d8857c24d5ecf5d7b869b45
iamwave007 <iamwave007@hotmail.com>
2015-03-14 02:58:12 -0400

    changed

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ app.get('/',function(req,res){
   res.sendFile(__dirname+"/index.html");
 })
 
+var chat=io.of('/chat')
 io.of("/chat").on("connection",function(socket){
   console.log('a user connected');
   socket.on("chat message",function(msg){
     console.log("message: "+msg)
-    io.of("/chat").emit("chat message",msg);
-    //io.emit("chat message",msg);
+    // socket.emit("chat message",msg); // √√X only working sending back to itself, the socket where it's from!!!
+    // io.of("/chat").emit("chat message",msg);// √√ works same way as the line below
+    //chat.emit("chat message",msg); // √√√ working, to all io.of("/chat")
+    //io.emit("chat message",msg); // XXXX not working due to of('/chat') missing
   })
   socket.on("disconnect",function(){
     console.log("disconnected")
