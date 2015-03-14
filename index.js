e9ba07b6cb02730e4f52de52a92d98c6b06afb55
iamwave007 <iamwave007@hotmail.com>
2015-03-14 02:46:43 -0400

    socket namespacing working

--- index.js
+++ index.js
@@ -7,15 +7,28 @@ app.get('/',function(req,res){
   res.sendFile(__dirname+"/index.html");
 })
 
-io.on("connection",function(socket){
+io.of("/chat").on("connection",function(socket){
   console.log('a user connected');
-  // console.log(io)
   socket.on("chat message",function(msg){
     console.log("message: "+msg)
-    io.emit("chat message",msg);
-    
+    io.of("/chat").emit("chat message",msg);
+    //io.emit("chat message",msg);
+  })
+  socket.on("disconnect",function(){
+    console.log("disconnected")
   })
 })
+
+// io.on("connection",function(socket){
+  // console.log('a user connected');
+  // socket.on("chat message",function(msg){
+  //   console.log("message: "+msg)
+  //   io.emit("chat message",msg);
+  // })
+  // socket.on("disconnect",function(){
+  //   console.log("disconnected")
+  // })
+// })
 
 http.listen(4000,function(){
   console.log("listening on *:4000");
